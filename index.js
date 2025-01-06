ac25b41e4fb4b1e52f1ce027ffbe2fd2c7d16019
JHSiam <iamjhsiam@gmail.com>
2025-01-06 11:11:03 +0600

    env added and deployment done

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
-require('dotenv').config();
+
 
 const port = process.env.PORT || 5000;
 
@@ -13,7 +14,9 @@ const port = process.env.PORT || 5000;
 //0mwW4VeE24OcnDlF
 app.use(cors({
     origin: [
-        'http://localhost:5173'
+        'http://localhost:5173',
+        'https://join-marathon.web.app',
+        'https://join-marathon.firebaseapp.com'
     ],
     credentials: true
 }));
@@ -22,6 +25,9 @@ app.use(cookieParser());
 
 const verifyToken = (req, res, next) => {
     const token = req.cookies?.token;
+
+    console.log(token);
+    
 
     if (!token) {
         return res.status(401).send({ message: 'unauthorized access' });
@@ -39,7 +45,7 @@ const verifyToken = (req, res, next) => {
 
 
 
-//const uri = "mongodb+srv://iamjhsiam:0mwW4VeE24OcnDlF@cluster0.iyayy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.iyayy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -54,7 +60,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        //await client.connect();
         const database = client.db('MarathonDB');
         const UsersCollection = database.collection('MarathonCollection');
         const RegisterCollection = database.collection('RegisterCollection');
@@ -234,8 +240,8 @@ async function run() {
         //     res.send(result);
         // })
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        //await client.db("admin").command({ ping: 1 });
+        //console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
         //await client.close();
